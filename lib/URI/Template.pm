e1182e5a09ed150916084e25985670b210361903
Anthony Parsons <flussence@gmail.com>
2014-06-11 17:27:21 +0100

    Implement basic percent-encoding
    This is enough to run the two level 1 examples.

--- lib/URI/Template.pm
+++ lib/URI/Template.pm
@@ -3,8 +3,8 @@ class URI::Template:auth<github:flussence>:ver<0.0.1>;
 use URI::Template::Actions;
 use URI::Template::Grammar;
 
-has Str $.template;
-has     $.parsed;
+has Str   $.template;
+has Match $.parsed;
 
 method new(Str $template) {
     my $parsed = URI::Template::Grammar.parse(
@@ -18,17 +18,29 @@ method new(Str $template) {
 #= Creates a URI by filling in the parameters.
 method format(%params) returns Str {
     return join q{}, gather for $.parsed.ast {
-        say $_.perl;
-        when Str  { take $_ }
-        when Pair {
-            fail 'Level 2+ operators NYI' if .value;
-            fail 'Compound values NYI' if .key.elems > 1;
-            take ~%params{.key}
+        when Str    { take $_ }
+        when Array  {
+            fail 'Level 2+ operators NYI' if .[1];
+            fail 'Compound values NYI'    if .[0].elems > 1;
+            take pct-escape(%params{.[0]} // fail "Undefined variable {.[0]}");
         }
+        default     { ??? }
     }
 }
 
 #= Attempts to parse a list of variables out of a given URI.
 method parse(Str $uri) returns Array {
     ...
+}
+
+#= Escape URI characters
+sub pct-escape($str) {
+    # List borrowed from RFC 3986
+    my @allowed = 'a'..'z', 'A'..'Z', '0'..'9', qw{- . _ ~};
+
+    # Idea recycled from Str.indent (maybe there's a better way to do this)
+    $str.split(/ @allowed+ /, :all).map({
+        when Match { $_ }
+        when Str   { .ords».fmt('%%%02x').join }
+    }).join;
 }
