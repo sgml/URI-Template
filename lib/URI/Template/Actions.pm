250352bcee0c2e899777b2d4501019e81ce8e0bf
Anthony Parsons <ant@flussence.eu>
2014-06-11 14:54:24 +0100

    Optional things should go at the back of the AST
    It's an awkward setup either way, but this is slightly less confusing.

--- lib/URI/Template/Actions.pm
+++ lib/URI/Template/Actions.pm
@@ -3,9 +3,9 @@ class URI::Template::Actions:auth<github:flussence>:ver<0.0.1>;
 method TOP($/)          { make $<piece>».ast }
 method  piece($/)       { make $/.values[0].ast }
 method   literal($/)    { make ~$/ }
-method   expr($/)       { make $<op>.?ast => [$<var>».ast] }
+method   expr($/)       { make [$<var>».ast.item, $<op>.?ast] }
 method    op($/)        { make ~$/ }
 method     op-reserved  { ??? }
-method    var($/)       { make $<var-name>».ast }
+method    var($/)       { make [$<var-name>».ast.item, $<mod-lv4>.?ast] }
 method     var-name($/) { make ~$/ }
 method     mod-lv4($/)  { ??? }
